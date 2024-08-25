74e202362d377aed05ee6956b3424c544a04b531
km-saifullah <kmsaifullah16@gmail.com>
2024-08-25 19:21:10 +0600

    port setting changed

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const editBlogController = require("./controller/editBlogController");
 const deleteBlogController = require("./controller/deleteBlogController");
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
